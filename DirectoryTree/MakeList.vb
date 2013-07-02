6aaf0bba935b07920b78049ed53d3fd9f15e9c7a
AngelHacker <djh1017555@126.com>
2013-07-02 15:21:55 +0800

    增加获取指定路径下的所有目录和文件功能

--- DirectoryTree/MakeList.vb
+++ DirectoryTree/MakeList.vb
@@ -70,4 +70,30 @@ Public Class MakeList
     Public Function GetAllFile(myPath As String) As String()
         Return GetAllFileList(myPath).ToArray
     End Function
+    ''' <summary>
+    ''' 获取指定路径下的所有目录和文件
+    ''' </summary>
+    ''' <param name="myPath">指定路径</param>
+    ''' <param name="order">是否按层次遍历</param>
+    ''' <returns>指定路径下的所有目录和文件</returns>
+    ''' <remarks>含子目录中的目录和文件</remarks>
+    Public Function GetAllPathList(myPath As String, Optional order As Boolean = False) As List(Of String)
+        Dim ret As New List(Of String)
+        If order Then
+            Dim myCurrentFiles() As String = GetCurrentFiles(myPath)
+            For i = 0 To myCurrentFiles.Count - 1
+                ret.Add(myCurrentFiles(i))
+            Next
+            Dim myCurrentDirectories() As String = GetCurrentDirectories(myPath)
+            For i = 0 To myCurrentDirectories.Count - 1
+                ret.Add(myCurrentDirectories(i))
+                ret.AddRange(GetAllFileList(myCurrentDirectories(i)))
+            Next
+        Else
+            '先目录后文件
+            ret.AddRange(GetAllDirectoryList(myPath))
+            ret.AddRange(GetAllFileList(myPath))
+        End If
+        Return ret
+    End Function
 End Class
